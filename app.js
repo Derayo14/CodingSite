afccf58b6690c3dc60dae849282979f9d0bde768
Derayo14 <adedokunyusroh@gmail.com>
2023-11-06 09:53:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let date =  new Date() //jan 1 1970 = sat mar 3 1973
+
+console.log(date.getMinutes());
+
+// new Date(string )
+// new Date(year, month)
+// new Date(year, month, day)
+
+// ISO
+// 2015-03-05
+// Short Date Format
+// 03/25/2023
+// Long date format
+// mar 25 2023 
+
+// date get methods
+// getFullYear
+// getMonth
+// getDate
+// getDay
+// getHours
+
+// if statement in js
+
+
+// let number1 = "5"
+var age = prompt("AGE: ")
+// window.prompt("deeede")
+
+let buttonClicked = false
+
+if(age == 90){
+    console.log("Are you sure you want to submit");
+}else{
+    console.log("you are running out of time");
+}
+
+
+
+
+
+
